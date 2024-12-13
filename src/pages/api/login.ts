d88ff235ad86883fd860caa614089a6179f9baae
Clodomir Jr. <106870350+jrfurt@users.noreply.github.com>
2024-12-13 07:50:43 -0300

    integração com o db de usuário

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -1,16 +1,25 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { conectarMongoDB } from "../../../middlewares/conectarMongoDB";
-import type { respostaPadraoMsg } from "../../../types/respostaPadraoMsg";
+import type { RespostaPadraoMsg } from "../../../types/RespostaPadraoMsg";
+import { UsuarioModel } from "@/models/UsuarioModel";
+import md5 from "md5";
 
-const endpointLogin = (
+const endpointLogin = async (
   req: NextApiRequest,
-  res: NextApiResponse<respostaPadraoMsg> // <> padroniza o type da response
+  res: NextApiResponse<RespostaPadraoMsg> // <> padroniza o type da response
 ) => {
   if (req.method === "POST") {
     const { login, senha } = req.body;
 
-    if (login === "admin@admin.com" && senha === "Admin@123") {
-      return res.status(200).json({ msg: "Usuario autenticado com sucesso" });
+    const usuarioEncontrado = await UsuarioModel.find({
+      email: login,
+      senha: md5(senha)
+    });
+    if (usuarioEncontrado && usuarioEncontrado.length > 0) {
+      const usuarioLogado = usuarioEncontrado[0];
+      return res
+        .status(200)
+        .json({ msg: `Usuario ${usuarioLogado.nome} autenticado com sucesso` });
     }
 
     return res.status(400).json({ erro: "Usuario ou senha nao encontrado" });

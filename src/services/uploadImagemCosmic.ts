142e7937049bcc14d6359cd78c5e0713cb0b0e42
Clodomir Jr. <106870350+jrfurt@users.noreply.github.com>
2024-12-16 15:14:17 -0300

    adicionando service de conexão com cosmic js para envio de mídia

---
+++ src/services/uploadImagemCosmic.ts
@@ -0,0 +1,36 @@
+import multer from "multer";
+import { createBucketClient } from "@cosmicjs/sdk";
+
+const { BUCKET_SLUG, WRITE_KEY, READ_KEY } = process.env;
+
+const bucketDevagram = createBucketClient({
+  bucketSlug: BUCKET_SLUG as string,
+  readKey: READ_KEY as string,
+  writeKey: WRITE_KEY as string
+});
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+const uploadImagemCosmic = async (req: any) => {
+  if (req?.file?.originalname) {
+    const mediaObject = {
+      originalname: req.file.originalname,
+      buffer: req.file.buffer
+    };
+
+    if (req.url && req.url.includes("publicacao")) {
+      return await bucketDevagram.media.insertOne({
+        media: mediaObject,
+        folder: "publicacao"
+      });
+    } else {
+      return await bucketDevagram.media.insertOne({
+        media: mediaObject,
+        folder: "avatar"
+      });
+    }
+  }
+};
+
+export { upload, uploadImagemCosmic };

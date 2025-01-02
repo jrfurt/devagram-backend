b917f3dcec8c1ee95cd657a068517d7573e82fa7
Clodomir Jr. <106870350+jrfurt@users.noreply.github.com>
2025-01-02 15:56:05 -0300

    criação da rota de perfil de usuário

--- src/pages/api/usuario.ts
+++ src/pages/api/usuario.ts
@@ -1,8 +1,27 @@
+import { conectarMongoDB } from "@/middlewares/conectarMongoDB";
 import { validarTokerJWT } from "@/middlewares/validarTokerJWT";
+import { UsuarioModel } from "@/models/UsuarioModel";
+import { RespostaPadraoMsg } from "@/types/RespostaPadraoMsg";
 import { NextApiRequest, NextApiResponse } from "next";
 
-const usuario = (req: NextApiRequest, res: NextApiResponse) => {
-  return res.status(200).json({ msg: "Usuario autenticado com sucesso" });
+const endpointUsuario = async (
+  req: NextApiRequest,
+  res: NextApiResponse<RespostaPadraoMsg>
+) => {
+  try {
+    const { userId } = req.query;
+
+    const usuario = await UsuarioModel.findById(userId);
+    // Setar a senha como null ao retornar
+    usuario.senha = null;
+
+    return res.status(200).json(usuario);
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ erro: "Nao foi possivel obter dados do usuario" });
+  }
 };
 
-export default validarTokerJWT(usuario);
+export default validarTokerJWT(conectarMongoDB(endpointUsuario));

58924f3f84ca49dffb01abaa90105a5c1f334a01
Clodomir Jr. <106870350+jrfurt@users.noreply.github.com>
2025-02-02 10:56:44 -0300

    feat: adicionando a quantidade de publicações

--- src/models/UsuarioModel.ts
+++ src/models/UsuarioModel.ts
@@ -6,7 +6,8 @@ const usuarioSchema = new Schema({
   senha: { type: String, required: true },
   avatar: { type: String, required: false },
   seguidores: { type: Number, default: 0 },
-  seguindo: { type: Number, default: 0 }
+  seguindo: { type: Number, default: 0 },
+  publicacoes: { type: Number, default: 0 }
 });
 
 export const UsuarioModel =
--- src/pages/api/publicacao.ts
+++ src/pages/api/publicacao.ts
@@ -47,6 +47,9 @@ const handler = nextConnect()
         data: new Date()
       };
 
+      usuario.publicacoes++;
+      await UsuarioModel.findByIdAndUpdate({ _id: usuario._id }, usuario);
+
       await PublicacaoModel.create(publicacao);
       return res.status(200).json({ msg: "Publicacao criada com sucesso!" });
     } catch (error) {
